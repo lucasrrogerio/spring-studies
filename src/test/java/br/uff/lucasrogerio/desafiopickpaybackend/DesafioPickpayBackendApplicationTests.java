59405b7ae9830ddb6d338577a7aa0662f86179ae
Lucas Ferreira <lucas.ferreira@primeup.com.br>
2023-11-17 13:16:39 -0300

    Projeto inicial Spring

---
+++ src/test/java/br/uff/lucasrogerio/desafiopickpaybackend/DesafioPickpayBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.uff.lucasrogerio.desafiopickpaybackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioPickpayBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
